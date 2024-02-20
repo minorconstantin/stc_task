f6c39f0b3c54f5aeaf884b8cef8d2f8c15ffda7b
konstantin petukhov <minore0403@yandex.ru>
2024-02-20 20:25:18 +0300

    Добавлены функции чтения записи входных выходных данных

--- read_rite_files.cpp
+++ read_rite_files.cpp
@@ -1,6 +1,80 @@
-#include "read_rite_files.h"
+﻿#include "read_rite_files.h"
 
 read_rite_files::read_rite_files()
 {
-
+read_data.clear(); // на всякий случай очищаем файл с данными
+}
+void read_rite_files::set_error_read(short error) // функция сохранения ошибки
+{
+error_read = error;
+}
+void read_rite_files::set_read_data(double new_data) // функция добавления данных
+{
+read_data.push_back(new_data);
+}
+short read_rite_files::get_error_read ()
+{
+return  error_read ;
+}
+vector<double>  read_rite_files::get_read_data ()
+{
+return read_data ;
+}
+void read_rite_files::read_settings ()
+{
+ifstream setings_file ("setings.txt");
+if (!setings_file.is_open()) 
+{
+set_error_read (-1);
+return;
+}
+string string_temp; // строка для чтения файла
+while (getline(setings_file, string_temp))
+{
+istringstream for_convert(string_temp);
+double new_data;
+for_convert >> new_data;
+set_read_data(new_data);
+}
+setings_file.close();
+set_error_read(read_data.size());
+}
+string read_rite_files::convert_to_text (short error)
+{
+string text_error ;
+switch (error)
+{
+case -1:
+text_error  = "не удалось открыть файл";
+break;
+case 0:
+text_error   ="не удалось прочитать файл";
+break;
+case 15:
+text_error   ="данные успешно прочитаны";
+break;
+default:
+ostringstream temp_convert;
+temp_convert << fixed << error;
+text_error    = "не соответствие данных  прочитано " + temp_convert.str() + " а должно быть 15";
+}
+return text_error   ;
+}
+void read_rite_files::write_answer (vector<double>   result)
+{
+ofstream file_result ("coords.txt");
+if (!file_result .is_open()) 
+return;
+if (result.size() !=6)
+file_result << convert_to_text (get_error_read());
+else
+{
+file_result <<  "координата a по x =" << result.at(0) << endl;
+file_result <<  "координата a по y =" << result.at(1) << endl;
+file_result <<  "координата b по x =" << result.at(2) << endl;
+file_result <<  "координата b по y =" << result.at(3) << endl;
+file_result <<  "координата c по x =" << result.at(4) << endl;
+file_result <<  "координата c по y =" << result.at(5) << endl;
+}
+file_result.close();
 }
--- read_rite_files.h
+++ read_rite_files.h
@@ -1,11 +1,24 @@
-#ifndef READ_RITE_FILES_H
+﻿#ifndef READ_RITE_FILES_H
 #define READ_RITE_FILES_H
-
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+using namespace std;
 
 class read_rite_files
 {
 public:
-    read_rite_files();
+    read_rite_files(); // конструктор
+short get_error_read (); // получения статуса чтения файла
+vector<double>  get_read_data(); // функция получения входных данных
+void read_settings (); // функция чтения входных данных
+void write_answer (vector<double>  ); // функция записи результата
+private:
+short error_read; // маркер успешности прочитанного файла
+vector<double>  read_data; // вектор, хранящий входные данные
+void set_error_read(short); // сохранение ошибки чтения
+void set_read_data(double); //  добавление прочитанного в вектор
+string convert_to_text (short); // функция формирования текстового описания ошибки
 };
-
 #endif // READ_RITE_FILES_H
